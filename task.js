76d868012a02e82fbc3ad7643aab33b3f87a3769
Rom4exe <rom.exe@mail.ru>
2024-12-08 00:36:30 +0800

    edit_bhj-homeworks

---
+++ task.js
@@ -0,0 +1,23 @@
+let dead = document.getElementById('dead');
+let lost = document.getElementById('lost');
+getHole = index => document.getElementById(`hole${index}`);
+for (holeIndex = 1; holeIndex < 10 ; holeIndex++) {
+   let hole = getHole(holeIndex);
+   hole.onclick = () => {
+   if (hole.className == 'hole hole_has-mole') {
+   dead.textContent = Number(dead.textContent)+1}
+   else {lost.textContent = Number(lost.textContent)+1}
+   setTimeout(() => { 
+   if (dead.textContent == 10) {
+      alert ("Вы победили!");
+      dead.textContent = 0;
+      lost.textContent = 0;
+      }
+   else if (lost.textContent == 5) {
+      alert ("Вы проиграли");
+      dead.textContent = 0;
+      lost.textContent = 0;
+      }
+   }, 500)
+   }
+}
